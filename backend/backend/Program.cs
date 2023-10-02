b826c0165cef00282a71591d79490e273bc60da1
Thomasmp00 <thomaxx00@gmail.com>
2023-10-02 11:09:42 +0200

    klsdfkl

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,48 @@
+using backend;
+using backend.DAL;
+using backend.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+
+// Add services to the container.
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddNpgsqlDataSource(Utilities.ProperlyFormattedConnectionString,
+        dataSourceBuilder => dataSourceBuilder.EnableParameterLogging());
+}
+
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddNpgsqlDataSource(Utilities.ProperlyFormattedConnectionString);
+}
+
+
+// builder.Services.AddSingleton<BoxService>();
+// builder.Services.AddSingleton<BoxDAL>();
+
+builder.Services.AddScoped<BoxService>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(options =>
+{
+    options.SetIsOriginAllowed(origin => true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+});
+app.MapControllers();
 app.Run();
