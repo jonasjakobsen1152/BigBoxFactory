7612f72c014f97afbe5304ec08c73b94f1ead488
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-10-02 09:03:59 +0200

    Update home.page.ts

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template:``,
+  template:`
+  <div>
+
+
+  </div>
+  `,
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
   constructor() {}
 
+  
 }
