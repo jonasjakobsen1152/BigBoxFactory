9f9b2ca10782aebfe0fe56197269562b7ed1c1ca
Thomasmp00 <thomaxx00@gmail.com>
2023-10-04 12:35:03 +0200

    dgfklægd

--- frontend/src/app/boxdetails/boxdetails.component.ts
+++ frontend/src/app/boxdetails/boxdetails.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {MyService} from "../../MyService";
 import {Box} from "../../Interface";
 import {firstValueFrom} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-boxdetails',
@@ -16,12 +17,15 @@ import {firstValueFrom} from "rxjs";
 })
 export class BoxdetailsComponent {
 
-  constructor(private http: HttpClient, public service: MyService) {
-    //this.getBoxData();
+  constructor(private http: HttpClient, public service: MyService, private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      const boxId = params['id'];
+      this.getBoxData(boxId)
+    });
   }
 
-  async getBoxData(Box: Box){
-    const call = this.http.get<Box[]>('http://localhost:5000/boxes/boxdetails' + Box.id)
+  async getBoxData(boxid: number){
+    const call = this.http.get<Box[]>('http://localhost:5000/boxes/box' + boxid);
     const result = await firstValueFrom<Box[]>(call);
     this.service.boxes = result;
   }
