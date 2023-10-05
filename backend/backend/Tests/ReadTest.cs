bed08a88d57dfff9e9011118e6d082763707acd6
Andreas Eriksen <pjuske.10@live.dk>
2023-10-05 11:38:45 +0200

    hej

--- backend/backend/Tests/ReadTest.cs
+++ backend/backend/Tests/ReadTest.cs
@@ -1,6 +1,19 @@
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
 namespace backend.Tests;
 
-public class ReadTest
+public class ReadTest : PageTest
 {
+    [Test]
+    public async Task CanSuccessfullyRead()
+    {
+        
+        await Page.GotoAsync("http://localhost:4200/");
+
+        await Page.GetByText("Box Window").ClickAsync();
+        
+        
+    }
     
 }
---
+++ backend/backend/Tests/Searchtest.cs
@@ -0,0 +1,17 @@
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace backend.Tests;
+
+public class Searchtest : PageTest
+{
+    [Test]
+    public async Task CanSuccessfullySearch()
+    {
+        
+        
+        
+    }
+    
+    
+}
