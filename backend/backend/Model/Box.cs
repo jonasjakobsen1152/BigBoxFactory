8008c25fe5506af99d4dd4f2175ce027f39dbaa3
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-10-12 10:16:49 +0200

    Update Box.cs

--- backend/backend/Model/Box.cs
+++ backend/backend/Model/Box.cs
@@ -1,11 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace backend.Model;
 
 public class Box
 {
     public int Id { get; set; }
     
+    [Required]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Content must be between 3 and 20 characters")]
     public string Content { get; set; }
     
     public string Size { get; set; }
     
+}
+public class BoxSizeValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value != null)
+        {
+            string size = value.ToString();
+            if (size != "Small" && size != "Medium" && size != "Large")
+            {
+                return new ValidationResult("Size must be 'Small', 'Medium', or 'Large'.");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
 }
